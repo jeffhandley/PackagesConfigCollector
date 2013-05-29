b333d72f59884e16e1c31c2a1b639cf07c3e31a8
jeffhandley <jeff@jeffhandley.com>
2013-05-29 07:20:14 -0700

    Upload packages.config files to blob storage

--- PackageConfigCollector/Controllers/HomeController.cs
+++ PackageConfigCollector/Controllers/HomeController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace BootstrapMvcSample.Controllers
 {
@@ -24,16 +27,34 @@ namespace BootstrapMvcSample.Controllers
             }
             else
             {
+                var connectionString = ConfigurationManager.AppSettings["BlobConnectionString"];
+                var account = CloudStorageAccount.Parse(connectionString);
+                var client = account.CreateCloudBlobClient();
+                var container = client.GetContainerReference("configfiles");
+                container.CreateIfNotExists();
+
+                var permissions = new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Off };
+                container.SetPermissions(permissions);
+
                 foreach (var file in packagesConfig.Where(f => f != null))
                 {
-                    string fileName = Path.GetFileName(file.FileName);
-                    if (string.Compare(fileName, "packages.config", StringComparison.OrdinalIgnoreCase) == 0)
+                    var fileName = Path.GetFileName(file.FileName);
+                    if (file.ContentLength > 1024 * 1024)
                     {
-
+                        ModelState.AddModelError("", string.Format("Sorry, {0} was too big and we had to ignore it.", new HtmlString(fileName)));
                     }
                     else
                     {
-                        ModelState.AddModelError("", string.Format("We can only accept packages.config files. {0} was not accepted.", new HtmlString(fileName)));
+                        if (string.Compare(fileName, "packages.config", StringComparison.OrdinalIgnoreCase) == 0)
+                        {
+                            var stamp = DateTime.UtcNow.ToString("yyyy-MM-dd-HH-mm-ss-") + Guid.NewGuid().ToString("N").Substring(0, 6) + ".config";
+                            var blob = container.GetBlockBlobReference(stamp);
+                            blob.UploadFromStream(file.InputStream);
+                        }
+                        else
+                        {
+                            ModelState.AddModelError("", string.Format("We can only accept packages.config files. {0} was not accepted.", new HtmlString(fileName)));
+                        }
                     }
                 }
             }
